cc914ae322282ac4ce4d435f75d2f25235b2c229
David Berniell <davidberniell@gmail.com>
2019-09-17 20:29:04 +0200

    generate getters and setters for User Entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,7 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * A book.
+ * A user.
  *
  * @ORM\Entity
  */
@@ -42,4 +42,68 @@ class User
      * @ORM\Column(type="datetime")
      */
     private $updated_at;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return \DateTimeInterface
+     */
+    public function getCreatedAt(): \DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param \DateTimeInterface $created_at
+     */
+    public function setCreatedAt(\DateTimeInterface $created_at): void
+    {
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * @return \DateTimeInterface
+     */
+    public function getUpdatedAt(): \DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * @param \DateTimeInterface $updated_at
+     */
+    public function setUpdatedAt(\DateTimeInterface $updated_at): void
+    {
+        $this->updated_at = $updated_at;
+    }
 }
